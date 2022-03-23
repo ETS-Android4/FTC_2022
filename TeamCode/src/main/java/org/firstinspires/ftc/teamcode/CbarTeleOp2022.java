8fd210a9621b5afff0b0508db69a4c783f55aade
micha <michaelwebb370@gmail.com>
2022-03-23 01:55:13 -0500

    First teleOp code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CbarTeleOp2022.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CbarTeleOp2022.java
@@ -29,12 +29,13 @@
 
 package org.firstinspires.ftc.teamcode;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
+
 
 
 /**
@@ -50,65 +51,104 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
-@Disabled
-public class CbarTeleOp2022 extends LinearOpMode {
+@TeleOp(name="Comp Opmode", group="Practice")
 
-    // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+public class CbarTeleOp2022 extends OpMode {
+
+    //DriveTrain Motor Variables
+    DcMotor leftDrive;
+    DcMotor rightDrive;
+    DcMotor centerDrive;
+
+
+    //DriveTrain Motor power variables
+    double leftDrivePower;
+    double rightDrivePower;
+    double centerDrivePower;
+
+    //LiftSystem Motor variables
+    DcMotor liftMotor;
+    DcMotor leftIntake;
+    DcMotor rightIntake;
+
+    //LiftSystem Motor power variables
+    double liftPower;
+    double leftIntakePower;
+    double rightIntakePower;
+
+    //Carosel
+    DcMotor caroselMotor;
+
+    double caroselPower;
+
 
     @Override
-    public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+    public void init(){
+        //DriveTrain Hardware mapping
+        leftDrive=hardwareMap.dcMotor.get("leftDrive");
+        rightDrive=hardwareMap.dcMotor.get("rightDrive");
+        centerDrive=hardwareMap.dcMotor.get("centerDrive");
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
+
+        //Intake Hardware mapping
+        liftMotor=hardwareMap.dcMotor.get("liftMotor");
+        leftIntake=hardwareMap.dcMotor.get("leftIntake");
+        rightIntake=hardwareMap.dcMotor.get("rightIntake");
+
+        //Set directions
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        centerDrive.setDirection(DcMotor.Direction.FORWARD);
+        liftMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightIntake.setDirection(DcMotor.Direction.FORWARD);
+        leftIntake.setDirection(DcMotor.Direction.REVERSE);
+        caroselMotor.setDirection(DcMotor.Direction.FORWARD);
+    }
+    @Override
+    public void loop(){
+        leftDrivePower=-gamepad1.left_stick_y;
+        rightDrivePower=-gamepad1.right_stick_y;
 
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-        runtime.reset();
 
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
+        if(gamepad1.dpad_left==true){
+            centerDrivePower=1;
+        }
+        else if(gamepad1.dpad_right==true){
+            centerDrivePower=-1;
+        }
+        else{
+            centerDrivePower=0;
+        }
 
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPower;
-            double rightPower;
+        leftDrive.setPower(leftDrivePower);
+        rightDrive.setPower(rightDrivePower);
+        centerDrive.setPower(centerDrivePower);
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
+        liftPower=gamepad2.left_stick_y;
+        liftMotor.setPower(liftPower);
 
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x;
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+        //Intake
+        if(gamepad2.x==true){
+            leftIntake.setPower(1);
+            rightIntake.setPower(1);
+        }else if(gamepad2.y==true){
+            leftIntake.setPower(-1);
+            rightIntake.setPower(-1);
+        }else{
+            leftIntake.setPower(0);
+            rightIntake.setPower(0);
+        }
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
-
-            // Send calculated power to wheels
-            leftDrive.setPower(leftPower);
-            rightDrive.setPower(rightPower);
-
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-            telemetry.update();
+        //Carosel movement
+        if(gamepad2.a==true){
+            caroselMotor.setPower(1);
+        }
+        else if(gamepad2.b==true){
+            caroselMotor.setPower(-1);
+        }
+        else{
+            caroselMotor.setPower(0);
         }
     }
 }
